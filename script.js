8abf2742d98553094ae4e23a6e04efb532ad81ff
Shumaila <sayedshumaila27@gmail.com>
2024-08-24 20:11:43 +0530

    add initial files

---
+++ script.js
@@ -0,0 +1,29 @@
+
+       // Object Constructors
+        
+        // function Book(title, author, pages, read) {
+        //     this.title = title;
+        //     this.author = author;
+        //     this.pages = pages;
+        //     this.read = read;
+        //     this.info = function(){
+        //         return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+        //     }
+        // }
+
+        // const book1 = new Book('Atomic habits', 'James Clear', '323 pages', 'not read yet');
+
+        //     console.log(book1.title);
+        //     console.log(book1.author);
+        //     console.log(book1.pages);
+        //     console.log(book1.read);
+        //     console.log(book1.info());
+
+        // const book2 = new Book('Javascript for Dummies', 'Angel cooper', '456 pages', 'completed');
+        //     console.log(book2.title);
+        //     console.log(book2.author);
+        //     console.log(book2.pages);
+        //     console.log(book2.read);
+        //     console.log(book2.info());
+
+
