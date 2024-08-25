3bf504737cba8f3618f9b93060f2e8b04bf6970a
Shumaila <sayedshumaila27@gmail.com>
2024-08-25 14:49:17 +0530

    add new book through form

--- script.js
+++ script.js
@@ -27,3 +27,65 @@
         //     console.log(book2.info());
 
 
+
+        var form = document.querySelector('.card');
+        var bookList = document.querySelector('.main-section') ;
+        
+        
+
+        // form add book item
+        form.addEventListener('submit', addBook);
+
+        function addBook(e){
+              e.preventDefault();
+         
+          // get new book values
+         var newTitle = document.getElementById('title').value;
+         var newAuthor = document.getElementById('author').value;
+         var newPages = document.getElementById('pages').value;
+         var newStatus = document.querySelector('input[name="status"]:checked').value;
+       
+          // create new book element
+          var newBook = document.createElement('div');
+          // add class
+          newBook.className = 'book';
+
+          bookList.appendChild(newBook);
+
+         // create nbook display elements
+         //img
+         var removeIcon = document.createElement('img');
+         removeIcon.src = "assets/icons/close_24dp_121212_FILL1_wght400_GRAD0_opsz24.svg";
+         removeIcon.title = "Remove";
+         newBook.appendChild(removeIcon);
+
+         var bookDetails = document.createElement('div');
+         bookDetails.className = "book-details";
+
+         newBook.appendChild(bookDetails);
+
+         // display title
+         var displayTitle = document.createElement('h3');
+         displayTitle.textContent = newTitle;
+         bookDetails.appendChild(displayTitle);
+
+         // display author
+         var displayAuthor = document.createElement('p');
+         displayAuthor.textContent = `Author: ${newAuthor}`;
+         bookDetails.appendChild(displayAuthor);
+
+         // display page number
+         var displayPage = document.createElement('p');
+         displayPage.textContent = `Pages: ${newPages}`;
+         bookDetails.appendChild(displayPage);
+
+         // add button
+         var displayBtn = document.createElement('button');
+         displayBtn.textContent = newStatus;
+         bookDetails.appendChild(displayBtn);
+
+       //   div.appendChild(document.createElement(im))
+
+       //   var book = document.querySelector('.book');
+
+        }
